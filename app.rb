5539f783e90a4a367403c26bb695fb42a2c6c029
Grace Lu <155923103+ggracelu@users.noreply.github.com>
2025-04-22 05:58:30 +0000

    done

--- app.rb
+++ app.rb
@@ -14,6 +14,12 @@ get("/") do
 end
 
 get("/:first_symbol") do
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EX_RATE_KEY")}"
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies").keys #array of symbols like AED, AFN, etc
   @symbol_one = params.fetch("first_symbol")
   erb(:step_one)
 end
@@ -22,5 +28,13 @@ get("/:first_symbol/:second_symbol") do
   @from = params.fetch("first_symbol")
   @to = params.fetch("second_symbol")
 
+  @url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EX_RATE_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
+  @raw_response = HTTP.get(@url)
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+
   erb(:step_two)
+
 end
